ac1bb27384f71626d700622d9071654104d641c4
saaya-code <boualimohamed74@gmail.com>
2025-02-26 20:35:15 +0100

    added speedanalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from "./providers"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       <Providers>
         <>
           {children}
+          <SpeedInsights/>
           <Analytics/>
           <Toaster />
         </>
